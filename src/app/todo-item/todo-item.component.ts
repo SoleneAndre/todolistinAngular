112835cf3622b91c82832e3bdb7ed063fe1059b9
SoleneAndre <49842961+SoleneAndre@users.noreply.github.com>
2019-12-04 22:18:05 +0100

    add comm

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -17,6 +17,7 @@ declare var google: any;
 })
 export class TodoItemComponent implements OnInit {
   @Input()
+  // déclaration de tout les attributs ou objets dont on aura besoin
   private data: TodoItemData;
   private edite = false;
   private geocoder: any;
@@ -36,7 +37,7 @@ export class TodoItemComponent implements OnInit {
     }
   };
   private ajoutville = true;
-
+// pour le data binding on recupere les elements enfants donc issus du html
   @ViewChild('inputData', {static: false}) newTextInput: ElementRef;
   @ViewChild('gSearch', {static: false}) formSearch;
   @ViewChild('searchKey', {static: false}) hiddenSearchHandler;
@@ -60,66 +61,65 @@ export class TodoItemComponent implements OnInit {
     this.location.marker.draggable = true;
   }
 
-  getedite(): boolean {
+  getedite(): boolean { // pour savoir si la todo est edite ou non
     return this.edite;
   }
 
-  markerDragEnd($event: MouseEvent, item: TodoItemData) {
-    console.log($event);
-    this.data.location.marker.lat = $event.coords.lat;
-    this.data.location.marker.lng = $event.coords.lng;
+  markerDragEnd($event: MouseEvent, item: TodoItemData) { // pour bouger le marker sur la map
+    console.log($event); // on verifie le type d'evenement
+    this.data.location.marker.lat = $event.coords.lat; // on enregistre dans l item location la latitude
+    this.data.location.marker.lng = $event.coords.lng; // on enregistre dans l item location la longitude
     const latlng = {lat: $event.coords.lat, lng:  $event.coords.lng};
     this.geocoder.geocode({'location': latlng}, function(results, status) {
 
-
+// fonction qui prend en paramètre les coordonnees lat et long d'une position et qui retourne une adresse de type string
       if (status === 'OK') {
         if (results[0]) {
           // tslint:disable-next-line: no-shadowed-variable
           const res = results[0].formatted_address;
-          const span = document.getElementById('villechange');
+          const span = document.getElementById('villechange'); // on affiche directement sur l'item correspondant, l'adresse
             span.innerHTML = '' + res;
        }
       }
 
 });
-console.log(this.adresse);
+console.log(this.adresse); // on verifie l'adresse
 
   }
 
-  ajoutVille() {
+  ajoutVille() { // ajouter une ville à l'item
     const val = confirm('Voulez-vous ajouter une localisation?');
-    if (val === true) {
+    if (val === true) { // si l'utilisateur repond ok
   this.adresse = prompt('Ville de la nouvelle TODO :)', '');
            // Si l'adresse n'est pas vide
    if (this.adresse !== '') {
  if (!this.geocoder) {
-   this.geocoder = new google.maps.Geocoder();
+   this.geocoder = new google.maps.Geocoder(); // on accede a la methode Geocoder de la maps
   }
-    this.geocoder.geocode({
+    this.geocoder.geocode({ // il prend en parametre la string adresse que l'utilisateur a ajouté via le prompt
       'address': this.adresse
     }, (results, status) => {
       console.log(results);
-      if (status === google.maps.GeocoderStatus.OK) {
+      if (status === google.maps.GeocoderStatus.OK) { // fonction transformant l'adresse en latitude et longitude
         if (results[0].geometry.location) {
           this.location.lat = results[0].geometry.location.lat();
           this.location.lng = results[0].geometry.location.lng();
-          this.location.marker.lat = results[0].geometry.location.lat();
-          this.location.marker.lng = results[0].geometry.location.lng();
-          this.location.marker.draggable = true;
-          this.infoWindow = this.getlabel();
-          this.location.viewport = results[0].geometry.viewport;
+          this.location.marker.lat = results[0].geometry.location.lat(); // ajout de la latitude du marker
+          this.location.marker.lng = results[0].geometry.location.lng(); // ajout de la longitude du marker
+          this.location.marker.draggable = true; // pour pouvoir bouger le marker sur la carte
+          this.infoWindow = this.getlabel(); // afficher le nom de le todo en cliquant sur le marker
+          this.location.viewport = results[0].geometry.viewport; // surface de la fenetre de la map
         }
       } else {
-        alert('Sorry, this search produced no results.');
+        alert('Sorry, this search produced no results.'); // si il y a erreur sur l'adresse par exemple
       }
     });
     }
-    this.data.location.marker.lat = this.location.lat;
-    this.data.location.marker.lng = this.location.lng;
-  this.ajoutville = false;
-  // this.lienGoogle = 'https://www.google.com/maps/search/?api=1&query=' + this.adresse;
-  this.todoService.setlocalStoragevillemap(this.adresse);
-  this.lienGoogle = this.todoService.getlocalStoragevillemap();
+    this.data.location.marker.lat = this.location.lat; // on met a jour notre marker
+    this.data.location.marker.lng = this.location.lng; //
+  this.ajoutville = false; // on a termine d ajouter la ville si on veut modifier il faudra bouger le marker
+  this.todoService.setlocalStoragevillemap(this.adresse); // on met a jour le local Storage avec le nom de la ville
+  this.lienGoogle = this.todoService.getlocalStoragevillemap(); // 
   this.data.ville = this.adresse;
   console.log(this.lienGoogle);
     }
